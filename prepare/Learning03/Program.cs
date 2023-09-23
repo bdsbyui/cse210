18420ba02f91f01b6ecac46b4e6bdc995a4457ec
Bryant <bryant@7s4e.org>
2023-09-23 23:59:12 +0800

    W03 Prepare: Encapsulation Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction first = new();
+        Console.WriteLine(first.GetFractionString());
+        Console.WriteLine(first.GetDecimalValue());
+        
+        Fraction second = new(5);
+        Console.WriteLine(second.GetFractionString());
+        Console.WriteLine(second.GetDecimalValue());
+        
+        Fraction third = new(3, 4);
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
+
+        Fraction fourth = new(1, 3);
+        Console.WriteLine(fourth.GetFractionString());
+        Console.WriteLine(fourth.GetDecimalValue());
     }
 }
