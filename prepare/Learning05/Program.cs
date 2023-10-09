4b412234d209b1e507b0e620dad72bb52dc6def1
Bryant <bryant@7s4e.org>
2023-10-09 11:29:59 +0800

    W05 Prepare: Polymorphism Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new()
+        {
+            new Square("red", 2),
+            new Rectangle("yellow", 3, 4),
+            new Circle("blue", 5)
+        };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine
+                (
+                    $"The {shape.GetColor()} {shape.GetType().Name.ToLower()} has an area of {shape.GetArea()} square units."
+                );
+        }
     }
 }
