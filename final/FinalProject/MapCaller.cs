07dea981182e98cc90bed4e64cbfa2bf60e5dc03
Bryant <bryant@7s4e.org>
2023-10-22 06:16:37 +0800

    remove duplicative and extraneous code

--- final/FinalProject/MapCaller.cs
+++ final/FinalProject/MapCaller.cs
@@ -28,36 +28,6 @@ public class MapCaller : ApiCaller
         return forecast;
     }
 
-    public class MapResponse
-    {
-        public List<Result> results { get; set; }
-        public string status { get; set; }
-    }
-
-    public class Result
-    {
-        public List<AddressComponent> address_components { get; set; }
-        public string formatted_address { get; set; }
-        public Geometry geometry { get; set; }
-    }
-
-    public class AddressComponent
-    {
-        public string short_name { get; set; }
-        public List<string> types { get; set; }
-    }
-
-    public class Geometry
-    {
-        public Location location { get; set; }
-    }
-
-    public class Location
-    {
-        public double lat { get; set; }
-        public double lng { get; set; }
-    }
-
     private string GetAddressComponent
     (
         List<AddressComponent> components,
